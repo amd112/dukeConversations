992421eec6ea2e48fcfeea7773ff771b5bc1f4a0
Anne Driscoll <amd112@duke.edu>
2017-10-12 10:25:22 -0400

    ran all checks, organized code

--- code/test-sample.sql
+++ code/test-sample.sql
@@ -1,15 +1,14 @@
-CREATE VIEW Students_Data 
-SELECT netid, name, food_restrictions, phone_number, year, major, gender,
+CREATE VIEW Students_Data AS
+SELECT unique_id, netid, name, food_restrictions, phone_number, year, major, gender,
 	  (SELECT COUNT(*) FROM Applications WHERE s.unique_id = student_id) AS applied, 
-	  (SELECT COUNT(*) FROM Applications WHERE s.unique_id = student_id AND selected = 1) AS accepted,
-	  
+	  (SELECT COUNT(*) FROM Applications WHERE s.unique_id = student_id AND selected = 't') AS accepted
 FROM Students AS s;
 
-CREATE VIEW Professors_Data
+CREATE VIEW Professors_Data AS
 SELECT name, food_restrictions, 
 	  (SELECT COUNT(*) FROM Reviews WHERE dinner_id IN 
 		 (SELECT dinner_id FROM Dinners WHERE professor_id = p.unique_id)) AS num_reviews,
-	  (SELECT AVG(*) FROM Reviews WHERE dinner_id IN 
+	  (SELECT AVG(conversation_grade) FROM Reviews WHERE dinner_id IN 
 		 (SELECT dinner_id FROM Dinners WHERE professor_id = p.unique_id)) AS avg_reviews,
 	  (SELECT COUNT(*) FROM Dinners WHERE professor_id = p.unique_id) AS num_dinners
 FROM Professors as p;
