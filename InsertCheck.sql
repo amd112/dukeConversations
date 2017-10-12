c4891af452058e8432d494a9760c2715f0e63878
Summer Smith <summer.smith497@gmail.com>
2017-10-12 09:44:05 -0400

    Update InsertCheck.sql

--- InsertCheck.sql
+++ InsertCheck.sql
@@ -6,7 +6,7 @@ INSERT INTO Reviews VALUES
   (5, 11, 'Great Dinner', 4, 9); 
 
 --updating a key in applications 
-UPDATE Applications SET Student_id = 1 WHERE Student_id = 11;
+UPDATE Applications SET date_time = NULL WHERE Student_id = '2016-09-06 07:30:00';
 
 --make a dinner for a professor that doesn't exist 
 INSERT INTO Dinners VALUES 
@@ -22,13 +22,13 @@ INSERT INTO Reviews VALUES
 
 --assign a student a major that does not exist
 INSERT INTO Students VALUES 
-  (11, 'Alan Khaykin', NULL, 'ak374', 1487283948, 1, 1, 2019, 'HarryPotter', 'Male');
+  (11, 'Alan Khaykin', NULL, 'ak374', 1487283948, 2019,'HarryPotter', 'Male');
 
 --create a student with an invalid netID 
 INSERT INTO Students VALUES 
-  (12, 'Alan Khaykin', NULL, NULL, 1487283948, 1, 1, 2019, 'HarryPotter', 'Male');
-  (12, 'Alan Khaykin', NULL, 'sasas1118', 1487283948, 1, 1, 2019, 'HarryPotter', 'Male');
+  (12, 'Alan Khaykin', NULL, NULL, 1487283948, 2019, 'Public Policy', 'Male');
+  (12, 'Alan Khaykin', NULL, 'sasas1118', 1487283948, 2019,'Public Policy', 'Male');
 
 --create a student with a phone number that is not valid  
 INSERT INTO Students VALUES
-  (0101010, 'Alan Khaykin', 'none', 'abcdef', NULL, 1, 1, 2019);
+  (13, 'Alan Khaykin', NULL, 'ak374', NULL, 2019, 'Public Policy', 'Male');
