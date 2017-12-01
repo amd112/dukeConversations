e43430da8bcbe0344a5da3c2e9ce5b8bd0f44b5e
Anne Driscoll <amd112@duke.edu>
2017-12-01 15:07:43 -0500

    fixed data

--- legacy/past_data/munging.R
+++ legacy/past_data/munging.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-apps =  read_delim("~/Duke/DukeFall2017/CS 316/dukeConversations/past_data/raw_apps.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
+apps =  read_delim("~/Duke/DukeFall2017/CS 316/dukeConversations/legacy/past_data/raw_apps.txt", "\t", escape_double = FALSE, trim_ws = TRUE, na = character())
 apps = as.data.frame(apps)
 apps = select(apps, time, name, email, phone, year, major, reason, food, dinner, selected, skipped)
 apps = filter(apps, year == 2017)
@@ -14,13 +14,15 @@ apps$email = tolower(emails)
 
 
 #create student df
-names = unique(apps$name)
-student = data.frame(uniqueID = NA, name = NA, phone = NA, netid = NA, year = NA, major = NA, pronouns = NA, food = NA)
+names = unique(apps$email)
+uniqueids = sample(1000000:9999999, length(names))
+student = data.frame(username = NA, uniqueID = NA, name = NA, phone = NA, netid = NA, year = NA, major = NA, pronouns = NA, food = NA)
 i = 1
 for (n in names) {
-  temp = arrange(filter(apps, name == n), major)
-  student[i,]$uniqueID = i
-  student[i,c(2:6, 8)] = temp[1, c(2, 4, 3, 5, 6, 8)]
+  temp = arrange(filter(apps, email == n), major)
+  student[i,]$username = n
+  student[i,]$uniqueID = uniqueids[i]
+  student[i,c(3:7, 9)] = temp[1, c(2, 4, 3, 5, 6, 8)]
   i = i + 1
 }
 write.csv(student, file = "student.csv", row.names = FALSE)
