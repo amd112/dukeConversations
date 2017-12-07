da3218cc2a890bc02fce071cf822c04c01fe896c
nhb8 <32738464+nhb8@users.noreply.github.com>
2017-12-07 13:40:16 -0500

    Output CSV from Selection page

--- site/convos/app/admin.py
+++ site/convos/app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Student, Application, Professor, Review, Dinner, Selection, Attendance
 
-
 # Register your models here.
 
 
@@ -17,6 +16,24 @@ class SelectionAdmin(admin.ModelAdmin):
 	list_filter = ('dinner_id',)
 	list_editable = ('selected',)
 	list_per_page = 15
+	actions = ['download_csv']
+	def download_csv(self, request, queryset):
+		import csv
+		from django.http import HttpResponse
+		from io import StringIO
+
+		file = StringIO()
+		writer = csv.writer(file)
+		writer.writerow(['Username', 'Dinner ID', 'Interest', 'Application Count', 'Selection Percentage', 'Attendance Percentage', 'Selected'])
+		for r in queryset:
+			writer.writerow([r.username, r.dinner_id, r.interest, r.application_count, r.percent_selected, r.percent_attended, r.selected])
+
+		file.seek(0)
+		response = HttpResponse(file, content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename=selection_data.csv'
+		return response
+
+	download_csv.short_description = "Download a CSV File containing the selected rows"
 
 class AttendanceAdmin(admin.ModelAdmin):
 	list_display = ('username', 'dinner_id', 'attendance')
